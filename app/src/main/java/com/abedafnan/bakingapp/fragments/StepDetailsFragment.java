7dbfd837d50f1cebce9cfa37292a05dd51011bf2
Afnan A. Abed <abedafnan99@gmail.com>
2019-09-03 08:13:16 +0300

    feat: Add ExoPlayer to view the recipe step video

--- app/src/main/java/com/abedafnan/bakingapp/fragments/StepDetailsFragment.java
+++ app/src/main/java/com/abedafnan/bakingapp/fragments/StepDetailsFragment.java
@@ -1,5 +1,7 @@
 package com.abedafnan.bakingapp.fragments;
 
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,11 +13,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.abedafnan.bakingapp.R;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
@@ -27,6 +41,8 @@ public class StepDetailsFragment extends Fragment {
     private String mThumbnailURL;
     private String mDescription;
 
+    @BindView(R.id.tv_instructions_layout)
+    LinearLayout instructionsLayout;
     @BindView(R.id.tv_instructions)
     TextView instructionsTextView;
     @BindView(R.id.thumbnailView)
@@ -35,6 +51,13 @@ public class StepDetailsFragment extends Fragment {
     SimpleExoPlayerView simpleExoPlayerView;
     @BindView(R.id.media_frame)
     FrameLayout mediaFrameLayout;
+
+    private SimpleExoPlayer mExoPlayer;
+    private long mPlaybackPosition;
+    private boolean mPlayWhenReady;
+
+    public static final String PLAYBACK_POSITION = "playPosition";
+    public static final String PLAY_WHEN_READY = "playWhenReady";
 
     @Nullable
     @Override
@@ -49,16 +72,18 @@ public class StepDetailsFragment extends Fragment {
             mDescription = args.getString("description");
         }
 
-        Log.d("StepDetailFragment", "Fragment Created");
-        Log.d("StepDetailFragment", "VIDEO: " + mVideoURL + "");
-        Log.d("StepDetailFragment", "IMAGE: " + mDescription + "");
-        Log.d("StepDetailFragment", "DESC: " + mThumbnailURL + "");
+        Log.d("EXO", String.valueOf(mPlaybackPosition));
+        Log.d("EXO", String.valueOf(mPlayWhenReady));
 
-        // Start the video if available
+        if (savedInstanceState != null) {
+            mPlaybackPosition = savedInstanceState.getLong(PLAYBACK_POSITION);
+            mPlayWhenReady = savedInstanceState.getBoolean(PLAY_WHEN_READY);
+        } else {
+            mPlayWhenReady = true;
+        }
+
         if (TextUtils.isEmpty(mVideoURL)) {
             simpleExoPlayerView.setVisibility(View.GONE);
-        } else {
-            // TODO: Implement ExoPlayer and initialize it
         }
 
         // Set the step thumbnail image if available
@@ -80,6 +105,64 @@ public class StepDetailsFragment extends Fragment {
             instructionsTextView.setText(mDescription);
         }
 
+        // Display the player in full view when orientation is landscape
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            instructionsLayout.setVisibility(View.GONE);
+        }
+
         return rootView;
+    }
+
+    /**
+     * Initialize ExoPlayer.
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            simpleExoPlayerView.setPlayer(mExoPlayer);
+
+            // Prepare the MediaSource.
+            MediaSource mediaSource = new ExtractorMediaSource.Factory
+                    (new DefaultHttpDataSourceFactory(getString(R.string.app_name))).createMediaSource(mediaUri);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(mPlayWhenReady);
+            mExoPlayer.seekTo(mPlaybackPosition);
+        }
+    }
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mPlaybackPosition = mExoPlayer.getCurrentPosition();
+        mPlayWhenReady = mExoPlayer.getPlayWhenReady();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!TextUtils.isEmpty(mVideoURL)) {
+            initializePlayer(Uri.parse(mVideoURL));
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(PLAYBACK_POSITION, mPlaybackPosition);
+        outState.putBoolean(PLAY_WHEN_READY, mPlayWhenReady);
     }
 }
